59a787487b9cb64c557b3a384f4c59f31d27789a
quintron <olivier.drevillon@gmail.com>
2015-09-26 14:18:47 +0200

    tridiag solve : use of pointer for buffer

--- src/quant-core-algo/quant-core-algo/TridiagonalMatrix.cs
+++ src/quant-core-algo/quant-core-algo/TridiagonalMatrix.cs
@@ -5,29 +5,28 @@
         #region private fields
         private static unsafe void SolveTridiagonal(double* x, int size, double* a, double* b, double* c)
         {
-            //TODO faire le cas n=1 !!!
             if (size == 1)
             {
                 x[0] /= b[0];
                 return;
             }
 
-            double[] cprime = new double[size];
-
-            cprime[0] = c[0] / b[0];
-            x[0] = x[0] / b[0];
-
-            // loop from 1 to N - 1 
-            for (int i = 1; i < size; i++)
+            double[] buffer = new double[size];
+            fixed (double* pBuffer = &buffer[0])
             {
-                double m = 1.0 / (b[i] - a[i] * cprime[i - 1]);
-                cprime[i] = c[i] * m;
-                x[i] = (x[i] - a[i] * x[i - 1]) * m;
-            }
+                pBuffer[0] = c[0] / b[0];
+                x[0] = x[0] / b[0];
 
-            // loop from N - 2 to 0 inclusive, safely testing loop end condition 
-            for (int i = size - 1; i-- > 0;)
-                x[i] = x[i] - cprime[i] * x[i + 1];
+                for (int i = 1; i < size; i++)
+                {
+                    double m = 1.0 / (b[i] - a[i] * pBuffer[i - 1]);
+                    pBuffer[i] = c[i] * m;
+                    x[i] = (x[i] - a[i] * x[i - 1]) * m;
+                }
+
+                for (int i = size - 1; i-- > 0;)
+                    x[i] = x[i] - pBuffer[i] * x[i + 1];
+            }
         }
         private static unsafe void MultTridiagonal(double* x, int size, double* a, double* b, double* c)
         {
@@ -36,17 +35,17 @@
                 x[0] *= b[0];
                 return;
             }
-
-            double xPrevious = x[0];
+            
+            double prev = x[0];
             x[0] = b[0] * x[0] + c[0] * x[1];
 
             for (int i = 1; i < size - 1; ++i)
             {
                 double temp = x[i];
-                x[i] = a[i] * xPrevious + b[i] * x[i] + c[i] * x[i + 1];
-                xPrevious = temp;
+                x[i] = a[i] * prev + b[i] * x[i] + c[i] * x[i + 1];
+                prev = temp;
             }
-            x[size - 1] = a[size - 1] * xPrevious + b[size - 1] * x[size - 1];
+            x[size - 1] = a[size - 1] * prev + b[size - 1] * x[size - 1];
         }
         #endregion
 
